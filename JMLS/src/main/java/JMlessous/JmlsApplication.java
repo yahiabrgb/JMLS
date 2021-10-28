8cd6f4b78f376a957b5cc01f1bef691d6137926a
Bourguiba <Bourguiba@LAPTOP-M4ICSUSJ>
2021-10-28 21:28:59 +0200

    First Commit

---
+++ JMLS/src/main/java/JMlessous/JmlsApplication.java
@@ -0,0 +1,13 @@
+package JMlessous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmlsApplication.class, args);
+	}
+
+}
---
+++ JMLS/src/test/java/JMlessous/JmlsApplicationTests.java
@@ -0,0 +1,13 @@
+package JMlessous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
